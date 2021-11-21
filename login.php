2a0514d93e7fae11b4799a7fcf021ec1f92bc2b0
Caimzao <84990264+Caimzao@users.noreply.github.com>
2021-11-21 03:05:56 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include('conexao.php');
+
+/* não tenho mta certeza dessa parte, 
+se algm perceber que ta errado
+me avisa que eu arrumo.
+As partes do banco são imporoviso,
+vou provavelmente editar depois. */ 
+
+if(empty($_POST['usuario']) || empty($_POST['senha'])) {
+	header('Location: '); /* a definir */
+	exit();
+}
+
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$query = "select nome from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+
+$result = mysqli_query($conexao, $query);
+
+$row = mysqli_num_rows($result);
+
+if($row == 1) {
+	$usuario_bd = mysqli_fetch_assoc($result);
+	$_SESSION['nome'] = $usuario_bd['nome'];
+	header('Location: '); /* a definir */ 
+	exit();
+} else {
+	$_SESSION['nao_autenticado'] = true;
+	header('Location: '); /* a deifnir */
+	exit();
+}
