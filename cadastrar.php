ca24af1842048dc485c7f480ceae5a2c03aba7a9
Caimzao <84990264+Caimzao@users.noreply.github.com>
2021-11-22 00:49:35 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -2,29 +2,24 @@
 session_start();
 include("conexao.php");
 
-/* não tenho mta certeza dessa parte, 
-se algm perceber que ta errado
-me avisa que eu arrumo.
-Tem umas partes improvisadas */ 
-
 $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
 $usuario = mysqli_real_escape_string($conexao, trim($_POST['usuario']));
-$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha']))); /* criptografia */
+$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha']))); 
 
-$sql = "select count(*) as total from usuario where usuario = '$usuario'"; /* rever bd pra ter ctz, fiz isso meio improvisado */
+$sql = "select count(*) as total from usuario where usuario = '$usuario'"; 
 $result = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($result);
 
-if($row['total'] == 1) { /* informa se usuário já existe */
+if($row['total'] == 1) { 
 	$_SESSION['usuario_existe'] = true;
-	header('Location: cadastro.html'); /* redireciona */
+	header('Location: cadastro.html'); 
 	exit;
 }
 
-$sql = "INSERT INTO usuario (nome, usuario, senha, data_cadastro) VALUES ('$nome', '$usuario', '$senha', NOW())"; /* rever bd pra ter ctz, fiz isso meio improvisado */
+$sql = "INSERT INTO usuario (nome, usuario, senha, data_cadastro) VALUES ('$nome', '$usuario', '$senha', NOW())"; 
 
 if($conexao->query($sql) === TRUE) {
-	$_SESSION['status_cadastro'] = true; /* cadastro realizado */
+	$_SESSION['status_cadastro'] = true; 
 }
 
 $conexao->close();
