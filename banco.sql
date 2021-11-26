5b5cec77d5e9c1b0a32ee5a8158d5c4b45600393
Caimzao <84990264+Caimzao@users.noreply.github.com>
2021-11-25 21:31:45 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -6,7 +6,6 @@ CREATE TABLE login.usuario (
   usuario VARCHAR(200) NOT NULL,
   senha VARCHAR(32) NOT NULL,
   nome VARCHAR(100) NOT NULL,
-  data_cadastro DATETIME NOT NULL,
   PRIMARY KEY (usuario_id));
 
-INSERT INTO usuario (usuario_id,usuario,senha, nome, data_cadastro) VALUES (1,'canalti','10f722b5984a49bce67d434464fae37e', 'Canal TI', '2019-01-11 19:42:12');
+INSERT INTO usuario (usuario_id,usuario,senha, nome) VALUES (1,'todo','', 'To Do');
