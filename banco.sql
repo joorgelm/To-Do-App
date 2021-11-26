a0239838919f3b52721a9ae7e15c3744ce85c71c
Caimzao <84990264+Caimzao@users.noreply.github.com>
2021-11-25 21:39:35 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -6,6 +6,7 @@ CREATE TABLE login.usuario (
   usuario VARCHAR(200) NOT NULL,
   senha VARCHAR(32) NOT NULL,
   nome VARCHAR(100) NOT NULL,
+  data_cadastro DATETIME NOT NULL,
   PRIMARY KEY (usuario_id));
 
-INSERT INTO usuario (usuario_id,usuario,senha, nome) VALUES (1,'todo','', 'To Do');
+INSERT INTO usuario (usuario_id,usuario,senha, nome, data_cadastro) VALUES (1,'todo','', 'To Do', '25-11-2021 20:35:00');
